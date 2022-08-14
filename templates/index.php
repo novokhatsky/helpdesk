61e00d5b3c599c11187f610252fb7938d300af44
novokhatsky <program_maker@mail.ru>
2022-08-14 13:48:41 +0700

    обязательное заполнение формы при создании заявки

--- templates/index.php
+++ templates/index.php
@@ -14,17 +14,17 @@
             <form method="post">
                 <div class="form-group">
                     <label for="fio">ФИО:</label>
-                    <div><input type="text" name="fio" id="fio" class="form-control"></div>
+                    <div><input type="text" name="fio" id="fio" class="form-control" required></div>
                 </div>
                 <div class="form-group">
                     <label for="cabinet">Номер кабинета:</label>
-                    <div><input type="text" name="cabinet" id="cabinet" class="form-control"></div>
+                    <div><input type="text" name="cabinet" id="cabinet" class="form-control" required></div>
                 </div>
                 <div class="form-group">
                     <div>Тип услуги:</div>
                     <div>
-                        <select name="id_service" class="form-control" >
-                            <option>выберите тип заявки</option>
+                        <select name="id_service" class="form-control" required>
+                            <option value="">выберите тип заявки</option>
                             <?php foreach ($service as $row) { ?>
                                 <option value="<?=$row['id_service']?>"><?=$row['name']?></option>
                             <?php } ?>
@@ -33,7 +33,7 @@
                 </div>
                 <div class="form-group">
                     <label for="description">Описание технической проблемы:</label>
-                    <textarea name="description" id="description" class="form-control" row="5"></textarea>
+                    <textarea name="description" id="description" class="form-control" row="5" required></textarea>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Отправить</button>
